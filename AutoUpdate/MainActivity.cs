6bdeaebffc4cf226149251230eb6570344629649
gwise <gwise@GWISE-PC>
2020-03-02 11:30:45 +0900

    1

--- AutoUpdate/MainActivity.cs
+++ AutoUpdate/MainActivity.cs
@@ -13,18 +13,16 @@ namespace AutoUpdate
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        int REQUEST_INSTALL_PERMISSION = 10;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
             var apkPath = FilesDir.AbsolutePath + "/app.apk";
 
-            using (File fileDel = new File(apkPath.ToString()))
+            using (Java.IO.File fileDel = new Java.IO.File(apkPath.ToString()))
             {
                 if (!fileDel.Exists())
                 {
@@ -34,26 +32,23 @@ namespace AutoUpdate
 
             InstallApk();
 
-            if (PackageManager.CanRequestPackageInstalls())
-            {
-                InstallApk();
-            }
+            //if (PackageManager.CanRequestPackageInstalls())
+            //{
+            //    InstallApk();
+            //}
            
         }
-        
+
         private void InstallApk()
         {
-            //var apkPath = FilesDir.AbsolutePath + "/app.apk";
-            var apkUri = FileProvider.GetUriForFile(
-                                                    this.ApplicationContext,
-                                                    "com.gwise.autoupdate.fileprovider",
-                                                    new Java.IO.File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads) + "app.apk")
-                                                    );
+            var apkPath = FilesDir.AbsolutePath + "/app.apk";
+            var apkUri = Android.Support.V4.Content.FileProvider.GetUriForFile(this.ApplicationContext, string.Format("{0}{1}", Application.Context.PackageName, ".fileprovider"), new Java.IO.File(apkPath));
 
             Intent intent = new Intent(Intent.ActionView);
             intent.AddFlags(ActivityFlags.GrantReadUriPermission | ActivityFlags.NewTask);
-            intent.SetDataAndType(apkUri, "application/vnd.android.package-archive");
-            
+            intent.PutExtra(Intent.ExtraNotUnknownSource, true);
+            intent.SetDataAndType(apkUri, this.ContentResolver.GetType(apkUri));
+
             StartActivity(intent);
         }
 
@@ -72,23 +67,6 @@ namespace AutoUpdate
 
             inputStream.Close();
             outputStream.Close();
-        }
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-
-            //if (requestCode == OPEN_DIRECTORY_REQUEST_CODE && resultCode == Result.Ok)
-            //{
-            //    this.ContentResolver.TakePersistableUriPermission(data.Data, ActivityFlags.GrantReadUriPermission | ActivityFlags.GrantWriteUriPermission);
-            //}
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
